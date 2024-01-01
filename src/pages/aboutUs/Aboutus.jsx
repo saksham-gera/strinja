c28eb4d9e45c89b821d67ab55f6eefe03643bb63
Omramanuj <ramanuj.om.r@gmail.com>
2024-01-01 20:59:32 +0530

    updated about us

--- src/pages/aboutUs/Aboutus.jsx
+++ src/pages/aboutUs/Aboutus.jsx
@@ -38,6 +38,35 @@ export default function Aboutus() {
             </div>
 
         </div>
+
+        <div className='vis'>
+            <div className='vis-header'>
+              <div className="vis-title">VISION</div>
+            </div>
+
+            <div className='vis-body'>
+              <p>Our vision is to transcend traditional boundaries, pushing the boundaries of string artistry through continuous innovation and a relentless pursuit of aesthetic mastery.  </p>
+              <p> Empowering homes and spaces with the enduring beauty of string art, we envision Strinja as a leading global brand synonymous with artistic excellence and meaningful cratfsmanship. </p>
+            </div>
+
+        </div>
+
+        <div className='pas'>
+            <div className='pas-header'>
+              <div className="pas-title">PASSION</div>
+              <div className='pas-writing'>Drive to Commitment to STRINJA</div>
+            </div>
+
+            <div className='pas-body'>
+              <p></p>
+              <p>At Strinja, our unwavering commitment to string art is fueled by an intrinsic passion for creativity, craftsmenship, and the transformative power of art. </p>
+              <p> We believe that every strand has the potential to tell a unique story. </p>
+              <p></p>
+              <p>We are not just creators of string art, we are enthusiasts who find joy in the process and fulfillment in knowing that our art resonates with the hearts and minds of those who encounter it.  </p>
+              <p></p>
+            </div>
+
+        </div>
           
        <Footer/>
       </>
